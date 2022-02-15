bf486fafa32f30dc900cf0822dc48b1c1d6be0bf
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-02-15 22:58:32 +0700

    prepare the handle todo function using Todo model

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import "./App.css"
 import InputField from './components/InputField';
+import { Todo } from './model';
 
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, {
+        id: Date.now(),
+        todo: todo,
+        isDone: false
+      }]);
+      setTodo("");
+    }
+  }
 
   useEffect(() => {
-    console.log("===== todo:", todo)
-  }, [todo]);
+    console.log("==== todos:", todos)
+  }, [todos]);
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={ todo } setTodo={ setTodo } />
+      <InputField 
+        todo={ todo } 
+        setTodo={ setTodo } 
+        handleAdd={ handleAdd }
+      />
     </div>
   );
 };
